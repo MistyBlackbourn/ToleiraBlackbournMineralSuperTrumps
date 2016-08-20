ab6c590268d791b53ffa1ca1605c2e4d42d56168
MistyBlackbourn <toleira.dyer@my.jcu.edu.au>
2016-08-20 15:16:02 +1000

    -Added MineralSuperTrumpsGamePlay class -This is where the main that runs the game is -Added menu to tell how many players and add their names

---
+++ src/MineralSuperTrumpsGamePlay.java
@@ -0,0 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MineralSuperTrumpsGamePlay {
+    public static void main(String[] args) {
+        String playerName;
+        int numberPlayers = 0;
+
+        do {
+            try {
+                Scanner userInput = new Scanner(System.in);
+                System.out.println("There must be at least 3 players and at most 5 players.");
+                System.out.print("Please enter the number of players: ");
+                numberPlayers = userInput.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Not a number");
+
+            }
+        } while (numberPlayers < 3 || numberPlayers > 5);
+
+        Scanner userInput = new Scanner(System.in);
+        for(int i = 0; i < numberPlayers; ++i) {
+            System.out.print("Please enter the name of player" + (i + 1) + ": ");
+            playerName = userInput.next();
+            System.out.println("Player" + (i + 1) + "'s name is " + playerName);
+        }
+
+    }
+}
